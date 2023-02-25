79a00d769f98fa63081ce10b60958924981cb216
Caleb-Lykken <lykken@uw.edu>
2023-02-25 03:30:14 -0800

    finished

--- Chart_1.R
+++ Chart_1.R
@@ -13,6 +13,7 @@ checkout_years_EBOOK <- cb_data %>%
   summarize(EBOOKS = sum(Checkouts))
 
 checkout_years <- left_join(checkout_years_BOOK, checkout_years_EBOOK, by = "CheckoutYear")
+checkout_years <- checkout_years %>% filter(CheckoutYear != 2023)
 
 df <- checkout_years %>%
   select(CheckoutYear, EBOOKS, BOOKS) %>%
@@ -22,4 +23,4 @@ df <- checkout_years %>%
 ggplot(df, aes(x = CheckoutYear, y = Number_of_checkouts)) + 
   geom_line(aes(color = Type_of_book, linetype = Type_of_book)) + 
   scale_color_manual(values = c("darkred", "steelblue")) +
-  ggtitle("Ebooks VS books checked out from 2005 to 2023")
+  ggtitle("Ebooks VS books checked out from 2005 to 2022")
--- Chart_3.R
+++ Chart_3.R
@@ -1,3 +1,16 @@
 library(dplyr)
 library(ggplot2)
+seasons <- c("Winter", "Winter", "Spring", "Spring", "Spring", "Summer", "Summer", "Summer","Fall", "Fall", "Fall" ,"Winter")
 cb_data <- read.csv("Cooking_Books.csv")
+cb_data <- cb_data %>% mutate(Season = seasons[CheckoutMonth])
+season_df <- cb_data %>% group_by(Season) %>% summarize(total_checkouts = sum(Checkouts))
+season_df <- season_df %>% mutate(percentage = round(total_checkouts/sum(total_checkouts)*100))
+
+ggplot(season_df, aes(x = "", y= total_checkouts, fill=Season)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0)  +  
+  geom_text(aes(label = paste0(percentage, "%")), position = position_stack(vjust=0.5)) +
+  ggtitle("Seasons that books are checked out between 2005 and 2023")
+
+
+  
